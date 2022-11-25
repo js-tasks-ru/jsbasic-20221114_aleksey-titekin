825ebf793484af0ecfef41da2a0c4623d7a52705
Aleksey Titekin <titekin.a@yandex.ru>
2022-11-25 11:05:22 +0300

    Module 2 - completed

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sumResult = 0;
+
+  for (let key in salaries) {
+    if (
+      typeof salaries[key] === "number" &&
+      Boolean(salaries[key]) &&
+      !(salaries[key] === Infinity || salaries[key] === -Infinity)
+    ) {
+      sumResult += salaries[key];
+    }
+  }
+  return sumResult;
 }
