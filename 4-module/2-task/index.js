6b3398ca27bc2f739ed1d17fabbb9a875cf5bd81
Aleksey Titekin <Titekin.A@yandex.ru>
2022-12-03 01:49:19 +0300

    task 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let i = 0;
+  for(let row of table.rows) {
+    row.cells[i++].style.backgroundColor = 'red';
+
+  //for (let i = 0 ; i < table.rows.length; i++) {
+  //  table.rows[i].cells[i].style.backgroundColor = 'red' ;
+  }
 }
