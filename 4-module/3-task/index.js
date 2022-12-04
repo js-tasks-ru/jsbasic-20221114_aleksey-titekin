b22ab9a152e57868d429b21ae386596a2a451506
Aleksey Titekin <Titekin.A@yandex.ru>
2022-12-04 11:03:27 +0300

    task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,19 @@
 function highlight(table) {
-  // ваш код...
+  for (let tbody of table.tBodies) {
+    for (let row of tbody.rows) {
+      if (!row.cells[3].hasAttribute("data-available")) {
+        row.hidden = true;
+        continue;
+      } 
+      row.style.textDecoration = +row.cells[1].textContent <= 18 ? "line-through" : "";
+      let cN;
+      cN =
+        row.cells[3].getAttribute("data-available") === "true"
+          ? "available"
+          : "unavailable";
+      cN += row.cells[2].textContent === "m" ? " male" : " female";
+      row.className = cN;
+      
+    }
+  }
 }
