4defbd678585a6ff04675ed07cd173acd7a83e1d
Aleksey Titekin <Titekin.A@yandex.ru>
2022-12-04 11:24:17 +0300

    4 module is ready

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,19 +1,16 @@
 function highlight(table) {
   for (let tbody of table.tBodies) {
     for (let row of tbody.rows) {
-      if (!row.cells[3].hasAttribute("data-available")) {
-        row.hidden = true;
-        continue;
-      } 
-      row.style.textDecoration = +row.cells[1].textContent <= 18 ? "line-through" : "";
-      let cN;
-      cN =
+      row.classList.add(
         row.cells[3].getAttribute("data-available") === "true"
           ? "available"
-          : "unavailable";
-      cN += row.cells[2].textContent === "m" ? " male" : " female";
-      row.className = cN;
+          : "unavailable"
+      );
+      row.classList.add(row.cells[2].textContent === "m" ? "male" : "female");
+
+      row.style.textDecoration = +row.cells[1].textContent <= 18 ? "line-through" : "";
       
+      if (!row.cells[3].hasAttribute("data-available")) row.hidden = true;
     }
   }
 }
