a5feed7c46b1374a50a1d56f739b8e969d7991ec
Aleksey Titekin <titekin.a@yandex.ru>
2022-12-05 15:46:59 +0300

    bug fix in task 2 and 3

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,9 +1,5 @@
 function makeDiagonalRed(table) {
-  let i = 0;
-  for(let row of table.rows) {
-    row.cells[i++].style.backgroundColor = 'red';
-
-  //for (let i = 0 ; i < table.rows.length; i++) {
-  //  table.rows[i].cells[i].style.backgroundColor = 'red' ;
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = "red";
   }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,15 +2,15 @@ function highlight(table) {
   for (let tbody of table.tBodies) {
     for (let row of tbody.rows) {
       row.classList.add(
-        row.cells[3].getAttribute("data-available") === "true"
+          row.cells[3].dataset.available === "true"
           ? "available"
           : "unavailable"
-      );
+      );      
       row.classList.add(row.cells[2].textContent === "m" ? "male" : "female");
 
       row.style.textDecoration = +row.cells[1].textContent <= 18 ? "line-through" : "";
       
-      if (!row.cells[3].hasAttribute("data-available")) row.hidden = true;
+      if (!('available' in row.cells[3].dataset)) row.hidden = true;
     }
   }
 }
