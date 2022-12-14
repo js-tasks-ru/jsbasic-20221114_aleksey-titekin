7a8a00ac57a3369f507b6be5b0db4e4432a61b83
Aleksey Titekin <titekin.a@yandex.ru>
2022-12-14 13:53:27 +0300

    6 module is completed

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,56 +1,50 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
-  #name; // название товара
-  #price; // цена товара
-  #category; // категория, к которой он относится, нам это понадобится чуть позже
-  #image; // название картинки товара
-  #id; //идентификатор
+  #product; //данные по товару
   #elem; // указатель на корневой элемент
   constructor(product) {
-    this.#name = product.name;
-    this.#price = product.price;
-    this.#category = product.category;
-    this.#image = product.image;
-    this.#id = product.id;
-
-    this.#elem = this.#createElem();
+    this.#product = product;
+    this.#elem = this.#createElemHTML();
   }
 
-  #createElem() {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let cardTop = document.createElement("div");
-    cardTop.classList.add("card__top");
-    card.append(cardTop);
-    let imgTop = document.createElement("img");
-    imgTop.src = `/assets/images/products/${this.#image}`;
-    imgTop.alt = "product";
-    imgTop.classList.add("card__image");
-    cardTop.append(imgTop);
+  #createElemHTML() {
+    let elem = createElement(
+      `      
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${
+            this.#product.image
+          }" class="card__image" alt="product">
+          <span class="card__price">€${this.#product.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.#product.name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+     `
+    );
 
-    let span = document.createElement("span");
-    span.classList.add("card__price");
-    span.innerHTML = '€' + this.#price.toFixed(2);
-    cardTop.append(span);
-
-    let cardBody = document.createElement("div");
-    cardBody.classList.add("card__body");
-    card.append(cardBody);
-    let cardTitle = document.createElement("div");
-    cardTitle.classList.add("card__title");
-    cardTitle.textContent =this.#name;
-    cardBody.append(cardTitle);
-    let btn = document.createElement("button");
-    btn.classList.add("card__button");
-    cardBody.append(btn);
-    let imgBtn = document.createElement("img");
-    imgBtn.src = "/assets/images/icons/plus-icon.svg";
-    imgBtn.alt = "icon";
-    btn.append(imgBtn);
-
-    return card;
+    let btn = elem.querySelector("button");
+    btn.addEventListener("click", (event) => {
+      this.#onClickCardButton(event);
+    });
+    return elem;
   }
 
   get elem() {
     return this.#elem;
+  }
+
+  #onClickCardButton(event) {
+    event.target.dispatchEvent(
+      new CustomEvent("product-add", {
+        detail: this.#product.id, 
+        bubbles: true, 
+      })
+    );
   }
 }
